1a3c0179fe2b7fca0222803d8a45bec65a3ecec7
Oleg Desheulin <ya.hef@yandex.ru>
2018-12-17 20:34:24 +0300

    small fix

--- bot/add_meeting.py
+++ bot/add_meeting.py
@@ -93,8 +93,8 @@ def add_workspace_to_meeting(bot, update):
 def add_location_to_meeting(bot, update):
     user = update.message.from_user
     logger.info("updated workspace for %s: %s", user.first_name, update.message.text)
-    wspace = last_message(user.id).text
     add_user_message(update)
+    wspace = last_message(user.id).text
     update.message.reply_text('Great! Now I need to know location!')
     items = ''
     for i, loc in enumerate(most_popular_locations(wspace)):
